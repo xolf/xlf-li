e392882e7c660cc8d8ae3823327ab3fdb47928b8
xolf <webmaster@xolf.info>
2016-04-27 17:28:34 +0100

    First Function

---
+++ src/Xlf/Url.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Xolf
+ */
+class Url
+{
+
+    public function get($url = null, $source = false)
+    {
+        $url = 'https://xlf.li?t=' . $url;
+        if($source) $url .= '&?s=' . $source;
+        return $source;
+    }
+
+}
+
+
+ ?>
