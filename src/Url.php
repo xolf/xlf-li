34cad25b656cc7b757fc392f213eaf33b0db80f2
xolf <webmaster@xolf.info>
2016-04-28 17:46:11 +0100

    New Url::link

--- src/Url.php
+++ src/Url.php
@@ -8,6 +8,12 @@ namespace Xolf\XlfLi;
 class Url
 {
 
+    public function link($url = null, $text = $url, $source = false)
+    {
+        $link = self::get($url, $source);
+        return '<a href="' . $link . '">' . $url . '</a>';
+    }
+
     public function get($url = null, $source = false)
     {
         $url = 'http://xlf.li?t=' . $url;
@@ -16,6 +22,5 @@ class Url
     }
 
 }
-
 
  ?>
