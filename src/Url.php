a7a9751c75d1bf9e756bb96d0d3b5fc329d4c9b1
xolf <xolf@users.noreply.github.com>
2017-02-19 16:15:29 +0100

    Update Url.php

--- src/Url.php
+++ src/Url.php
@@ -8,14 +8,14 @@ namespace Xolf\XlfLi;
 class Url
 {
 
-    public function link($url = null, $text = false, $source = false)
+    public static function link($url = null, $text = false, $source = false)
     {
         if(!$text) $text = $url;
         $link = self::get($url, $source);
         return '<a href="' . $link . '" rel="nofollow">' . $url . '</a>';
     }
 
-    public function get($url = null, $source = false)
+    public static function get($url = null, $source = false)
     {
         $url = 'http://xlf.li?t=' . $url;
         if($source) $url .= '&s=' . $source;
